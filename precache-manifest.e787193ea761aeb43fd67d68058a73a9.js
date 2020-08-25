9734e9831357a51d024dfc06071687aede7ffb24
Shalu Kumari <shaluraics22@gmail.com>
2020-08-25 20:21:58 +0530

    Updates

---
+++ precache-manifest.e787193ea761aeb43fd67d68058a73a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8953cfd2a9dcc04e76c8a0c886a0f80a",
+    "url": "/movies-world/index.html"
+  },
+  {
+    "revision": "c7a41119682afc1eadfb",
+    "url": "/movies-world/static/css/main.27d545f7.chunk.css"
+  },
+  {
+    "revision": "456f55456137c937c9e2",
+    "url": "/movies-world/static/js/2.58117173.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-world/static/js/2.58117173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a41119682afc1eadfb",
+    "url": "/movies-world/static/js/main.09c7ab99.chunk.js"
+  },
+  {
+    "revision": "73a2052431f57cc9c855",
+    "url": "/movies-world/static/js/runtime-main.625da92f.js"
+  },
+  {
+    "revision": "a3be25fafda1e673294afe627d0a2d9b",
+    "url": "/movies-world/static/media/image1.a3be25fa.jpg"
+  }
+]);
